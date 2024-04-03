43273387bbc07f13eac31c4dc805c6b072dd8ffa
root27 <oguzhaneee@gmail.com>
2024-04-03 12:02:56 +0300

    added screenshots

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 This is a simple tool that generates Github Actions yml file using user inputs. This project uses "gopkg.in/yaml.v2" for golang struct to yml conversion.
 
 
+## Screenshots
+
+1. Welcome Screen
+
+![Welcome Screen](./screenshots/mainScreen.png)
+
+2. User Inputs
+
+![User Inputs](./screenshots/UserInputs.png)
+
+3. Generated Yml
+
+![Generated Yml](./screenshots/output.png)
+
 
 ## LICENSE
 
--- main.go
+++ main.go
@@ -17,14 +17,22 @@ func Init() {
 	fmt.Println(green("Welcome to the GitHub Actions Workflow Generator"))
 
 	welcome_message := `
-		This tool will help you create a GitHub Actions workflow file for your repository.
-		You will be prompted to enter the workflow name, trigger event, branch name, and the number of steps you would like to add to the workflow.
-		You will also be prompted to enter the uses, name, and run command for each step.
-		You can also add a secret to the workflow if you wish.
-		You can also edit the workflow file after it has been generated.
+		- This tool will help you create a GitHub Actions workflow file for your repository.
+		
+		- You will be prompted to enter the workflow name, trigger event, branch name, and the number of steps you would like to add to the workflow.
+		
+		- You will also be prompted to enter the uses, name, and run command for each step.
+		
+		- You can also add a secret to the workflow if you wish.
+		
+		- You can also edit the workflow file after it has been generated.
 		`
 
+	ps := color.New(color.FgRed).Add(color.Underline).PrintlnFunc()
+
 	fmt.Println(cyan(welcome_message))
+
+	ps("P.S.: If you use secret in your workflow, make sure to add the secret to your repository with the same name. Also you have to add run command to use the secret in your workflow.\n")
 
 }
 
